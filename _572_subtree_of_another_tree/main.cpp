2d3fc492f31ca3fa5e097a7e8e93651d3ae95b24
Mathis Boultoureau <contact@mboultoureau.com>
2023-10-04 15:14:37 +0200

    add 2nd solution to 572

--- _572_subtree_of_another_tree/main.cpp
+++ _572_subtree_of_another_tree/main.cpp
@@ -11,6 +11,7 @@
  */
 class Solution {
 public:
+    // SOLUTION 1
     bool isSubtreeLevel(TreeNode* root, TreeNode* subRoot, int level) {
         if (subRoot == NULL && root == NULL)
         {
@@ -42,4 +43,39 @@ public:
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
         return isSubtreeLevel(root, subRoot, 0);
     }
+
+    // SOLUTION 2 (with NeetCode)
+    // bool isSameSubtree(TreeNode* root, TreeNode* subRoot)
+    // {
+    //     if (subRoot == NULL && root == NULL)
+    //     {
+    //         return true;
+    //     }
+
+    //     if (subRoot == NULL || root == NULL)
+    //     {
+    //         return false;
+    //     }
+
+    //     if (subRoot->val != root->val)
+    //     {
+    //         return false;
+    //     }
+
+    //     return isSameSubtree(root->left, subRoot->left) && isSameSubtree(root->right, subRoot->right);
+    // }
+
+    // bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+    //     if (root == NULL)
+    //     {
+    //         return false;
+    //     }
+
+    //     if (isSameSubtree(root, subRoot))
+    //     {
+    //         return true;
+    //     }
+
+    //     return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+    // }
 };
