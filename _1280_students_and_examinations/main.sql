a4f89a74bf129b2e823107bd980f8271710dbdb6
Mathis Boultoureau <contact@mboultoureau.com>
2023-11-09 23:13:13 +0100

    1280. Students and Examinations

---
+++ _1280_students_and_examinations/main.sql
@@ -0,0 +1,5 @@
+SELECT Students.student_id, Students.student_name, Subjects.subject_name, COUNT(Examinations.student_id) AS attended_exams FROM Students
+CROSS JOIN Subjects
+LEFT JOIN Examinations ON Examinations.student_id = Students.student_id AND Examinations.subject_name = Subjects.subject_name
+GROUP BY Students.student_id, Subjects.subject_name
+ORDER BY Students.student_id, Subjects.subject_name;
