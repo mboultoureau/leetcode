7d7e650e1abb8ddbe21f98a2e71106250499d24b
Mathis Boultoureau <contact@mboultoureau.com>
2023-11-23 20:44:37 +0100

    1141. User Activity for the Past 30 Days I

---
+++ _1141_user_activity_for_the_past_30_days_i/main.sql
@@ -0,0 +1,4 @@
+SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date < "2019-07-27" AND activity_date > DATE_SUB('2019-07-27', INTERVAL 30 DAY)
+GROUP BY activity_date
