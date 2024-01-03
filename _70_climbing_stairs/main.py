bf8e85332226f5ada047576cd8a79fb3327c2b8b
Mathis Boultoureau <contact@mboultoureau.com>
2024-01-03 18:53:35 +0100

    70. Climbing Stairs

--- _70_climbing_stairs/main.py
+++ _70_climbing_stairs/main.py
@@ -1,17 +1,31 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # Solution 3 : use less memory
+        # Solution 4
         if n <= 2:
             return n
 
-        prev = 2
-        curr = 3
-        for _ in range(n - 3):
-            new = prev + curr
-            prev = curr
-            curr = new
+        previous_one = 1
+        previous_two = 2
 
-        return curr
+        for _ in range(2, n):
+            current = previous_one + previous_two
+            previous_one = previous_two
+            previous_two = current
+
+        return previous_two
+
+        # Solution 3 : use less memory
+        # if n <= 2:
+        #     return n
+
+        # prev = 2
+        # curr = 3
+        # for _ in range(n - 3):
+        #     new = prev + curr
+        #     prev = curr
+        #     curr = new
+
+        # return curr
 
         # Solution 2
         # numbers = [1, 2]
