28e33e46f610a9f32bc87b0f8825f6dd82d3fa7e
Mathis Boultoureau <contact@mboultoureau.com>
2024-02-05 08:46:01 +0100

    387. First Unique Character in a String

---
+++ _387_first_unique_character_in_a_string/main.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int count[26] = {0};
+
+        for (int i = 0; i < s.size(); i++) {
+            count[s[i] - 'a'] += 1;
+        }
+        
+        for (int i = 0; i < s.size(); i++) {
+            if (count[s[i] - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
