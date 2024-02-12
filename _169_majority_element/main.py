f49c491982c5fff89ebbe2cc58f1959f2b64e24a
Mathis Boultoureau <contact@mboultoureau.com>
2024-02-12 07:41:54 +0100

    169. Majority Element

--- _169_majority_element/main.py
+++ _169_majority_element/main.py
@@ -1,8 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        numbers = Counter(nums)
-        target = len(nums) // 2 + 1
+        # Solution 2
+        return Counter(nums).most_common(1)[0][0]
 
-        for k, v in numbers.items():
-            if v >= target:
-                return k
+        # Solution 1
+        # numbers = Counter(nums)
+        # target = len(nums) // 2 + 1
+
+        # for k, v in numbers.items():
+        #     if v >= target:
+        #         return k
