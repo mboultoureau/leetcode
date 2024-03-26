85b4f5f5fc962d9dfb8069fc507e762727ac3851
Mathis Boultoureau <contact@mboultoureau.com>
2024-03-26 20:51:17 +0100

    46. Permutations

---
+++ _46_permutations/main.py
@@ -0,0 +1,18 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+
+        if len(nums) == 1:
+            return [nums.copy()]
+
+        for i in range(len(nums)):
+            n = nums.pop(0)
+            permutations = self.permute(nums)
+
+            for permutation in permutations:
+                permutation.append(n)
+
+            result.extend(permutations)
+            nums.append(n)
+
+        return result
