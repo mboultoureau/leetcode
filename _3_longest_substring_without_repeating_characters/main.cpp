0f761b978493f23b93cfa956e9b7c6061e22af8b
Mathis Boultoureau <contact@mboultoureau.com>
2024-08-14 18:09:39 +0900

    3. Longest Substring Without Repeating Characters

--- _3_longest_substring_without_repeating_characters/main.cpp
+++ _3_longest_substring_without_repeating_characters/main.cpp
@@ -1,32 +1,58 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // Must faster than hashmaps
-        vector<int> letters(127, -1);
-        
-        int left = 0;
-        int right = 0;
-        int maxLength = 0;
+        std::array<int, 128> letters;
+        std::fill(letters.begin(), letters.end(), -1);
+        int start = 0;
+        int result = 0;
 
-        while (right < s.size())
+        for (int i = 0; i < s.size(); i++)
         {
-            if (letters[s[right]] != -1)
-            {
-                while (left < letters[s[right]])
-                {
-                    letters[s[left]] = -1;
-                    left++;
-                }
+            char letter = s[i];
 
-                left = letters[s[right]] + 1;
+            if (letters[letter] != -1)
+            {
+                for (int j = start; j < letters[letter]; j++)
+                {
+                    letters[j] = -1;
+                }
+                start = max(start, letters[letter] + 1);
             }
 
-            letters[s[right]] = right;
-            maxLength = max(right - left + 1, maxLength);
-
-            right++;
+            result = max(result, i - start + 1);
+            letters[letter] = i;
         }
 
-        return maxLength;
+        return result;
+
+
+
+        // Must faster than hashmaps
+        // vector<int> letters(127, -1);
+        
+        // int left = 0;
+        // int right = 0;
+        // int maxLength = 0;
+
+        // while (right < s.size())
+        // {
+        //     if (letters[s[right]] != -1)
+        //     {
+        //         while (left < letters[s[right]])
+        //         {
+        //             letters[s[left]] = -1;
+        //             left++;
+        //         }
+
+        //         left = letters[s[right]] + 1;
+        //     }
+
+        //     letters[s[right]] = right;
+        //     maxLength = max(right - left + 1, maxLength);
+
+        //     right++;
+        // }
+
+        // return maxLength;
     }
 };
