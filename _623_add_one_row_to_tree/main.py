180973a4adad1dfd38d2babb43716fdb216ddd42
Mathis Boultoureau <contact@mboultoureau.com>
2024-08-31 12:59:22 +0900

    623. Add One Row to Tree

--- _623_add_one_row_to_tree/main.py
+++ _623_add_one_row_to_tree/main.py
@@ -6,34 +6,67 @@
 #         self.right = right
 class Solution:
     def addOneRow(self, root: Optional[TreeNode], val: int, depth: int) -> Optional[TreeNode]:
+        # Solution 1
         if depth == 1:
-            newRoot = TreeNode(val=val, left=root, right=None)
-            return newRoot
+            return TreeNode(val, left=root)
 
-        current = 2
+        depth -= 2
+
         queue = deque([root])
-
-        while queue:
-            if current == depth:
-                while queue:
-                    node = queue.popleft()
-                    newNodeLeft = TreeNode(val=val, left=node.left, right=None)
-                    newNodeRight = TreeNode(val=val, left=None, right=node.right)
-
-                    node.left = newNodeLeft
-                    node.right = newNodeRight
-
-                return root
-            
-
+        while queue and depth > 0:
             length = len(queue)
-            current += 1
+            depth -= 1
             for _ in range(length):
                 node = queue.popleft()
+
                 if node.left:
                     queue.append(node.left)
 
                 if node.right:
                     queue.append(node.right)
+
+        while queue:
+            node = queue.popleft()
+
+            newNode = TreeNode(val, left=node.left)
+            node.left = newNode
+
+            newNode = TreeNode(val, right=node.right)
+            node.right = newNode
+
+        return root
+        
+
+
+        # Solution 2
+        # if depth == 1:
+        #     newRoot = TreeNode(val=val, left=root, right=None)
+        #     return newRoot
+
+        # current = 2
+        # queue = deque([root])
+
+        # while queue:
+        #     if current == depth:
+        #         while queue:
+        #             node = queue.popleft()
+        #             newNodeLeft = TreeNode(val=val, left=node.left, right=None)
+        #             newNodeRight = TreeNode(val=val, left=None, right=node.right)
+
+        #             node.left = newNodeLeft
+        #             node.right = newNodeRight
+
+        #         return root
+            
+
+        #     length = len(queue)
+        #     current += 1
+        #     for _ in range(length):
+        #         node = queue.popleft()
+        #         if node.left:
+        #             queue.append(node.left)
+
+        #         if node.right:
+        #             queue.append(node.right)
 
 
