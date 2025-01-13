8a4ce5c729dbe00a84901d84c415abd3bca2c7ea
Mathis Boultoureau <contact@mboultoureau.com>
2025-01-13 20:13:16 +0100

    3223. Minimum Length of String After Operations

--- _3223_minimum_length_of_string_after_operations/main.py
+++ _3223_minimum_length_of_string_after_operations/main.py
@@ -1,9 +1,19 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
+        # Solution 1
         count = Counter(s)
-        length = len(s)
+        total = 0
 
-        for letter in count:
-            length -= (count[letter] - 1) // 2 * 2
+        for c in count:
+            total += ((count[c] + 1) % 2) + 1
 
-        return length
+        return total
+
+        # Solution 2
+        # count = Counter(s)
+        # length = len(s)
+
+        # for letter in count:
+        #     length -= (count[letter] - 1) // 2 * 2
+
+        # return length
