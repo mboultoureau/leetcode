44a8762ae6cdfa04c8260f99b0b6dae4edccbe5b
Mathis Boultoureau <contact@mboultoureau.com>
2025-02-20 11:25:03 -0500

    1980. Find Unique Binary String

--- _1980_find_unique_binary_string/main.py
+++ _1980_find_unique_binary_string/main.py
@@ -1,6 +1,18 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        for i in range(2**len(nums)):
-            binary = format(i, 'b').rjust(len(nums[0]), '0')
-            if binary not in nums:
-                return binary
+        n = len(nums[0])
+        maximum = int('1' * n, 2)
+        numbers = set()
+
+        for num in nums:
+            numbers.add(int(num, 2))
+
+        for i in range(maximum + 1):
+            if i not in numbers:
+                return bin(i)[2:].zfill(n)
+
+        # Solution 1
+        # for i in range(2**len(nums)):
+        #     binary = format(i, 'b').rjust(len(nums[0]), '0')
+        #     if binary not in nums:
+        #         return binary
